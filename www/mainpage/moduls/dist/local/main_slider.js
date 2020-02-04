62555409b121fc70e0f5d26e2a2bff97465bee4f
kimdawoon <hec8897@naver.com>
2020-02-04 14:43:31 +0900

    no message

--- www/mainpage/moduls/dist/local/main_slider.js
+++ www/mainpage/moduls/dist/local/main_slider.js
@@ -1,38 +1,22 @@
-const mainSlider = {
-  template: `<div class="swipe-portfolio">
-  <div class="swiper-wrapper">
-      <div class="swiper-slide" v-for="list in lists">
-          <a v-bind:href="'portfolio_view.html?idx='+list.idx">
-              <div class="overf">
-                  <h2>{{list.portfolioStandard}}</h2>
-                  <p>{{list.tit}}</p>
-                  <span>자세히 보기</span>
-              </div>
-              <!-- <div class='slidImg' v-bind:style='background-image:url(list.portfolioImg)'></div> -->
-              <img v-bind:src="list.portfolioImg" alt="sample img">
-          </a>
-      </div>
-  </div>
-  <div class="swiper-button-next"><img src="images/icon_next.png" alt=""></div>
-  <div class="swiper-button-prev"><img src="images/icon_priv.png" alt=""></div>
-</div>`,
-  data() {
-      return {
-          lists:null
-      }
+var mainSlider = {
+  template: "         <div class=\"swipe-portfolio\">\n    <div class=\"swiper-wrapper\">\n        <div class=\"swiper-slide\" v-for=\"list in lists\">\n            <a v-bind:href=\"'portfolio_view.html?idx='+list.idx\" v-bind:style=\"'background:url('+list.portfolioImg+') no-repeat; background-size:cover'\">\n                <div class=\"overf\">\n                    <h2>{{list.portfolioStandard}}</h2>\n                    <p>{{list.tit}}</p>\n                    <span>\uC790\uC138\uD788 \uBCF4\uAE30</span>\n                </div>\n                <img src=\"images/blank_img2.png\" alt=\"blank\">\n            </a>\n        </div>\n    </div>\n    <!-- navi -->\n    <div class=\"swiper-button-next\"><img src=\"images/icon_next.png\" alt=\"\"></div>\n    <div class=\"swiper-button-prev\"><img src=\"images/icon_priv.png\" alt=\"\"></div>\n</div>",
+  data: function data() {
+    return {
+      lists: null
+    };
   },
-  created() {
-      const baseURI = 'api/get.portfolio.main.php';
-      axios.post(
-              baseURI, {
-                  mode: "Slider",
-              }
-          )
-          .then((result) => {
-              if (result.data.phpResult == "ok") {
-                  this.lists = result.data.result
-              }
-          })
-          .catch(err => console.log('Login: ', err));
+  created: function created() {
+    var _this = this;
+
+    var baseURI = 'api/get.portfolio.main.php';
+    axios.post(baseURI, {
+      mode: "Slider"
+    }).then(function (result) {
+      if (result.data.phpResult == "ok") {
+        _this.lists = result.data.result;
+      }
+    }).catch(function (err) {
+      return console.log('Login: ', err);
+    });
   }
-}
+};
--- www/mainpage/moduls/vue/local/main_slider.js
+++ www/mainpage/moduls/vue/local/main_slider.js
@@ -1,18 +1,18 @@
 const mainSlider = {
-    template: `<div class="swipe-portfolio">
+    template: `         <div class="swipe-portfolio">
     <div class="swiper-wrapper">
         <div class="swiper-slide" v-for="list in lists">
-            <a v-bind:href="'portfolio_view.html?idx='+list.idx">
+            <a v-bind:href="'portfolio_view.html?idx='+list.idx" v-bind:style="'background:url('+list.portfolioImg+') no-repeat; background-size:cover'">
                 <div class="overf">
                     <h2>{{list.portfolioStandard}}</h2>
                     <p>{{list.tit}}</p>
                     <span>자세히 보기</span>
                 </div>
-                <div class='slidImg' v-bind:style='background-image:url(list.portfolioImg)'></div>
-                <!-- <img v-bind:src="list.portfolioImg" alt="sample img"> -->
+                <img src="images/blank_img2.png" alt="blank">
             </a>
         </div>
     </div>
+    <!-- navi -->
     <div class="swiper-button-next"><img src="images/icon_next.png" alt=""></div>
     <div class="swiper-button-prev"><img src="images/icon_priv.png" alt=""></div>
 </div>`,
